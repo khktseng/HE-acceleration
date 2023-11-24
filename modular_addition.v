a7c3da6a40b7e1a2e3e98437ed344d5b28fac818
kyle <khktseng@gmail.com>
2023-11-24 15:57:32 -0800

    Created modular_addition.v

---
+++ modular_addition.v
@@ -0,0 +1,13 @@
+module modular_addition(a, b, m, c);
+    parameter DATA_WIDTH = 8;
+
+    input [DATA_WIDTH-1:0] a;
+    input [DATA_WIDTH-1:0] b;
+    input [DATA_WIDTH-1:0] m;
+    output [DATA_WIDTH-1:0] c;
+
+
+
+
+
+endmodule
