19e0f770b582f9c65ebf63449422f11cfa4458d6
kyle <khktseng@gmail.com>
2023-11-24 16:11:43 -0800

    Changed to SystemVerilog

--- modular_addition.sv
+++ modular_addition.sv
@@ -6,8 +6,14 @@ module modular_addition(a, b, m, c);
     input [DATA_WIDTH-1:0] m;
     output [DATA_WIDTH-1:0] c;
 
+    wire [DATA_WIDTH-1:0] sum;
 
+    assign sum = a + b;
+    assign c =
 
-
-
+    always @(*) begin
+        c = a + b;
+        if (c > m)
+            c = c - m;
+    end
 endmodule
