4bc64269df97f5f9a1ba163d45807e51ce528a73
kyle <khktseng@gmail.com>
2023-11-25 21:50:12 -0800

    Finished simple test in mod add tb

--- tb_modular_addition.sv
+++ tb_modular_addition.sv
@@ -7,7 +7,14 @@ module tb_modular_addition;
 
     modular_addition(clk, a, b, m, c);
 
+    always #5 clk <= ~clk;
+
     initial begin
+        clk <= 0;
+        m = 9;
+        a = 8; b = 7; #10;
+        $display("a=%d, b=%d, m=%d, c=%d", a, b, m ,c);
+        $finish;
 
     end
 endmodule
