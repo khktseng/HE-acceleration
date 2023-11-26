d3050481c7136f6830e94118b791b79c005b09ae
kyle <khktseng@gmail.com>
2023-11-25 21:57:58 -0800

    fixes

--- modular_addition.sv
+++ modular_addition.sv
@@ -1,6 +1,7 @@
 module modular_addition(clk, a, b, m, c);
     parameter DATA_WIDTH = 8;
 
+    input clk;
     input [DATA_WIDTH-1:0] a;
     input [DATA_WIDTH-1:0] b;
     input [DATA_WIDTH-1:0] m;
@@ -11,7 +12,7 @@ module modular_addition(clk, a, b, m, c);
     always_comb begin
         out = a + b;
         if (out > m) begin
-            out = c - m;
+            out = out - m;
         end
     end
 
--- tb_modular_addition.sv
+++ tb_modular_addition.sv
@@ -5,15 +5,15 @@ module tb_modular_addition;
     logic clk;
     logic [DATA_WIDTH-1:0] a, b, m, c;
 
-    modular_addition(clk, a, b, m, c);
+    modular_addition DUT(clk, a, b, m, c);
 
     always #5 clk <= ~clk;
 
     initial begin
+        $monitor("clk=%b, a=%d, b=%d, m=%d, c=%d", clk, a, b, m ,c);
         clk <= 0;
         m = 9;
         a = 8; b = 7; #10;
-        $display("a=%d, b=%d, m=%d, c=%d", a, b, m ,c);
         $finish;
 
     end
