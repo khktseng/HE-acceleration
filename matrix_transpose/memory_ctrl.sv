a6f421de1a211881e38e408799899198dee6f598
Kyle Tseng <khktseng@gmail.com>
2024-02-21 22:49:05 -0800

    Created memory_ctrl

--- matrix_transpose/memory_ctrl.sv
+++ matrix_transpose/memory_ctrl.sv
@@ -1,5 +1,16 @@
 module memory_ctrl #(
+	parameter DATA_WIDTH = 64,
+	parameter BLOCK_SIZE = 8
+	parameter BANK_ADDR_WIDTH = $clog2(BLOCK_SIZE)
 )(
+	input clk,
+	input rst,
+
+	input ctrl
+
+	input val,
+	output ren,
+	output 
 )
 
 endmodule
